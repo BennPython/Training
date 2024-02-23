a4a10f258bc4177ed376c4111a0322614699bc67
BennPython <127740713+BennPython@users.noreply.github.com>
2024-02-23 11:48:04 +0000

    Update train3.py

--- train3.py
+++ train3.py
@@ -1,1 +1,30 @@
+a = ("John", "Charles", "Mike")
+b = ("Manager", "Supervisor", "Engineer")
+
+print a
+print b
+
+print "----   ZIP   ----"
+x = zip(a, b)
+print x
+
+city = 'Los Angeles'
+print city.find('x')
+
+
+print"---   dictionary {}    ---"
+fruit_qty = {}
+fruit_qty ['banana'] = 50
+fruit_qty ['apple'] = 78
+fruit_qty ['apricot'] = 40
+print fruit_qty
+fruit_qty ['orange'] = 70
+fruit_qty ['avocado'] = 30
+print fruit_qty
+print fruit_qty ['apricot']
+set_int = {1,2,3,4,5,67,5,4,3,21}
+print set_int
+set_int.add (9.5)
+print set_int
+teachers_set ={'mike', 'steve', 'hunter'}
 
